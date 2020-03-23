73d5f637e28f9858fbf92badcf1fc54c6520138c
정성오 <wnw1102@naver.com>
2020-03-23 09:03:47 +0000

    Update 0323/CDC.java

---
+++ 0323/CDC.java
@@ -0,0 +1,45 @@
+package medical;
+
+import java.util.List;
+import java.util.Set;
+
+import person.Patient;
+
+public class CDC extends Organization{
+	private List<Hospital> hospitalList;
+	private Set<Patient> patientList;
+	public CDC() {
+		super();
+	};
+	public CDC(String name, int employeeCount, List<Hospital> hospitalList, Set<Patient> patientList) {
+		super(name, employeeCount);
+		this.hospitalList = hospitalList;
+		this.patientList = patientList;
+	}
+	public List<Hospital> getHospitalList() {
+		return hospitalList;
+	}
+	public void setHospitalList(List<Hospital> hospitalList) {
+		this.hospitalList = hospitalList;
+	}
+	public Set<Patient> getPatientList() {
+		return patientList;
+	}
+	public void setPatientList(Set<Patient> patientList) {
+		this.patientList = patientList;
+	}
+	public void about() {
+		System.out.println("Organization - CDC " + super.getName());
+	}
+	public void about(String more) {
+		about();
+		System.out.println("We manage Hospital and Patients");
+	}
+	public void addPatient(Patient p)  throws NotCoronaException{
+		if(!p.isCorona()) throw new NotCoronaException("NotCorona");
+		this.patientList.add(p);
+	}
+	public void removePatient(Patient p) {
+		this.patientList.remove(p);
+	}
+}
