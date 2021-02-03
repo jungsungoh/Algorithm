6fa8c1d784cb2fa56ec4f0fd604c6ff306f56e7f
jungsungoh <wnw1102@naver.com>
2021-02-03 14:59:23 +0900

    algo

--- algorithm/src/자료구조/Mun_괄호의값.java
+++ algorithm/src/자료구조/Mun_괄호의값.java
@@ -1,12 +1,13 @@
 package 자료구조;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Mun_괄호의값 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
-		Stack<Char>
+		Stack<Char> asd
 		for(int i=0; i<s.length(); i++) {
 			if(s.charAt(i) == ')' || s.charAt(i) == ']') {
 				
